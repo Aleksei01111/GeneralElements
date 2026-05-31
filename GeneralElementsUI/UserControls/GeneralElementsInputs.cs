dedcffb154952bdf51650f4d6ad5df5120b687f6
aleksei01111 <alekseivolkov01012@gmail.com>
2026-05-31 18:55:30 +0300

    добавил события ввода на поля ввода

---
+++ GeneralElementsUI/UserControls/GeneralElementsInputs.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows.Controls;
+
+namespace GeneralElementsUI.UserControls;
+
+public class GeneralElementsInputs
+{
+    public delegate void GeneralInputsTextChangedEventHandler(object sender, TextChangedEventArgs e, string text);
+}
